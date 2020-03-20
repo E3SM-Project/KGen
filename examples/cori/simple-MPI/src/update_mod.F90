064c97b3d2d3a1417c12d7d9b74e8f6bec9a1d4a
Youngsung Kim <youngsung.kim.act2@gmail.com>
2020-03-20 11:33:06 -0700

    renamed Tulip to tulip and Summit to summit in examples; added cori examples

---
+++ examples/cori/simple-MPI/src/update_mod.F90
@@ -0,0 +1,39 @@
+MODULE update_mod
+    USE calc_mod, only : calc
+    PUBLIC update
+CONTAINS
+    SUBROUTINE update(rank, nranks)
+        INCLUDE 'mpif.h'
+        INTEGER, INTENT(IN) :: rank, nranks
+        INTEGER :: i, j, error
+        INTEGER :: lsum, gsum(nranks)
+        INTEGER, dimension(ROW, COL) :: output, out2, out3
+
+        gsum = 0
+
+        DO i=1, COL
+            DO j=1, ROW
+                output(j,i) = 0
+                out2(j,i) = 0
+                out3(j,i) = 0
+            END DO
+        END DO
+
+        DO i=1, COL
+            DO j=1, ROW
+                CALL calc(i, j, output, out2, out3)
+            END DO
+        END DO
+
+        lsum = SUM(output)
+
+        CALL mpi_gather(lsum, 1, MPI_INTEGER, &
+            gsum, 1, MPI_INTEGER, &
+            0, MPI_COMM_WORLD, error)
+
+        IF (rank == 0) THEN
+            print *, 'global sum = ', SUM(gsum)
+        END IF
+
+    END SUBROUTINE
+END MODULE
